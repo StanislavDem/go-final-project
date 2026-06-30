e89aaf463092c1051ea94fae2ef78a96f7f3dee4
StanislavDem <fars1r@outlook.com>
2026-06-30 19:04:55 +0500

    Fix m patch in go.mod & rfctr websrvr pkg

--- websrvr/srv.go
+++ websrvr/srv.go
@@ -1,4 +1,4 @@
-package main
+package websrvr
 
 import (
 	"fmt"
@@ -9,19 +9,15 @@ import (
     "github.com/StanislavDem/go-final-project/tests"
 )
 
-func main() {
+func StartServer() {
     // Директория для фронтенда
-    webDir := filepath.Join("..", "web")
+    webDir := filepath.Join("web")
 
-    // Возвращаем index.html при запросе "/"
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path == "/" {
-            http.ServeFile(w, r, filepath.Join(webDir, "index.html"))
-            return
-        }
-        // Для остальных файлов (css, js, изображения)
-        http.ServeFile(w, r, filepath.Join(webDir, r.URL.Path))
-    })
+    // FileServer возвращает index.html и вложенные файлы
+    fs := http.FileServer(http.Dir(webDir))
+
+    // StripPrefix для того чтобы "/" вело прямо в webDir
+    http.Handle("/", http.StripPrefix("/", fs))
 
     addr := ":" + fmt.Sprintf("%d", tests.Port) // порт берём из settings.go
     log.Printf("Server started on %s\n", addr)

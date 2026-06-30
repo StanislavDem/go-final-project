f5e93993acae1369750bbb926c38af28a4f47fe5
StanislavDem <fars1r@outlook.com>
2026-06-30 20:52:49 +0500

    Step2: Create SQLite DB init & updt settings.go & updt main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/StanislavDem/go-final-project/websrvr"
+import (
+	"log"
+
+	"github.com/StanislavDem/go-final-project/websrvr"
+	"github.com/StanislavDem/go-final-project/pkg/db"
+)
 
 func main() {
+	    if err := db.Init("dataBase/scheduler.db"); err != nil { // проверка на наличие scheduler.db
+        log.Fatalf("failed to init db: %v", err)
+    }
+	
     websrvr.StartServer()
 }

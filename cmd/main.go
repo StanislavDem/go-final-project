2b5534bce3b19ac18d2cb2b4c876e95b893f31ce
StanislavDem <fars1r@outlook.com>
2026-07-01 19:28:29 +0500

    Implement of the abil. to determ. path to the database file via the TODO_DBFILE env var

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/StanislavDem/go-final-project/websrvr"
 	"github.com/StanislavDem/go-final-project/pkg/db"
 )
 
 func main() {
-	    if err := db.Init("dataBase/scheduler.db"); err != nil { // проверка на наличие scheduler.db
+	// читаем переменную окружения TODO_DBFILE
+    dbFile := os.Getenv("TODO_DBFILE")
+    if dbFile == "" {
+    // если переменная не задана, используем значение по умолчанию
+        dbFile = "dataBase/scheduler.db"
+    }
+	if err := db.Init(dbFile); err != nil { // проверка на наличие dbFile
         log.Fatalf("failed to init db: %v", err)
     }
 	
